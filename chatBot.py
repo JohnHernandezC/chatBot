680adf9ce723b9ef3f737387f3bc5548e28751b2
Jhon Hernandez <johnher121099@gmail.com>
2022-02-08 17:41:05 -0500

    filtrado y divison del json

--- chatBot.py
+++ chatBot.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
+stemmer =LancasterStemmer()
 import numpy
 import tflearn
 import tensorflow
@@ -7,6 +8,8 @@ import json
 import random
 import pickle
 #nltk.download('punkt')
+
+#########INICIAR ARHIVO I FILTRAR LOS DATOS DENTRO###################
 with open("cont.json") as respuestas:
     datos= json.load(respuestas)
 print (datos)
@@ -23,7 +26,29 @@ for contenido in datos["contenido"]:
         auxiliar2.append(contenido["tag"])
         if contenido["tag"] not in tags:
             tags.append(contenido["tag"])
-print(palabras)
-print(tags)
-print(auxiliar1)
-print(auxiliar2)
+#####################################################################
+
+palabras=[stemmer.stem(w.lower())for w in palabras if w !="?"]
+palabras= sorted(list(set(palabras)))
+
+tags=sorted(tags)
+training=[]
+output=[]
+outputNull=[0 for  _ in range(len(tags))]
+
+for x, document in enumerate(auxiliar1):#x guarda el indice y document la palabra
+    cube=[]
+    auxWord=[stemmer.stem(w.lower())for w in document]
+    for w in palabras:
+        if w in auxWord:
+            cube.append(1)
+        else:
+            cube.append(0)
+    exitRow=outputNull[:]
+    exitRow[tags.index(auxiliar2[x])]=1
+    training.append(cube)
+    output.append(exitRow)
+    
+print(training)
+print(output)
+    
